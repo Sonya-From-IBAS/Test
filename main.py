bebf3611f75973db9697c13b030709c1d1947b96
Leonid <Anevskyi2018@yandex.ru>
2022-09-10 22:01:30 +0400

    add request to gismeteo

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
+import requests
 
+headers = {
+    'User-Agent': ('Mozilla/5.0 (Windows NT 6.0; rv:14.0) Gecko/20100101 '
+                   'Firefox/14.0.1'),
+    'Accept':
+    'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+    'Accept-Language':
+    'ru-ru,ru;q=0.8,en-us;q=0.5,en;q=0.3',
+    'Accept-Encoding':
+    'gzip, deflate',
+    'Connection':
+    'keep-alive',
+    'DNT':
+    '1'
+}
 
-print('Hello world!')
+url = f'https://www.gismeteo.ru/diary/4618/2022/8/'
+response = requests.get(url, headers=headers)
+print(response.status_code)
